d695951f2267b885974b02bc9f4f0ea97514dd91
popovic1 <djordje.popovic2001@gmail.com>
2024-08-27 22:00:45 +0200

    Everything works.

--- include/emulator.hpp
+++ include/emulator.hpp
@@ -7,30 +7,46 @@
 #include <sstream>
 #include <map>
 
+using namespace std;
+
 class Emulator
 {
 private:
-
+    string pc;
+    string sp;
+    vector<string> regs;
+    vector<string> csr;
+    vector<string> stack;
+    map<string, string> code;
 public:
     Emulator();
     ~Emulator();
-    std::map<std::string, std::string> code;
-    void readFile(std::string fileName);
+
+    void loadFromFile(string fileName);
+
+    void updatePC();
+    bool handleOpcode0(const std::string& opcode);
+    void handleOpcode1(const std::string& opcode);
+    void handleOpcode2(const std::string& opcode, const std::string& gprA, const std::string& gprB, const std::string& offset);
+    void handleBranching(const std::string& opcode, const std::string& gprA, const std::string& gprB, const std::string& gprC, const std::string& offset);
+    void handleSwap(const std::string& gprB, const std::string& gprC);
+    void handleArithmetic(const std::string& opcode, const std::string& gprA, const std::string& gprB, const std::string& gprC);
+    void handleLogical(const std::string& opcode, const std::string& gprA, const std::string& gprB, const std::string& gprC);
+    void handleShift(const std::string& opcode, const std::string& gprA, const std::string& gprB, const std::string& gprC);
+    void handleMemory(const std::string& opcode, const std::string& gprA, const std::string& gprB, const std::string& gprC, const std::string& offset);
+    void handleCSR(const std::string& opcode, const std::string& gprA, const std::string& gprB, const std::string& gprC, const std::string& offset);
+
+
     void emulate();
-    void movePC();
-    std::string calculateAddress(std::string gprA, std::string gprB, std::string D);
-    std::string getValueFromReg(std::string reg);
-    std::string convertFromLittleEndian(std::string value);
-    void setRegisterValue(std::string reg, std::string value);
-    std::string getValueFromCSR(std::string num);
-    void setValueForCSR(std::string num, std::string value);
-    std::string makeFourBytes(std::string value);
+
+    string calcIndAddr(string gprA, string gprB, string offset);
+    string getRegisterValue(string reg);
+    string toBigEndian(string val);
+    void setRegisterValue(string reg, string value);
+    string getCSRValue(string num);
+    void setCSRValue(string num, string value);
     void printRegisterValues();
-    std::string pc;
-    std::string sp;
-    std::vector<std::string> regs;
-    std::vector<std::string> csr;
-    std::vector<std::string> stack;
+
 };
 
 #endif
